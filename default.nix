87956fce7cd09838775bbeedc836e6d0b85c49ad
Sean Hickman <sean@grumply.com>
2020-02-14 00:02:18 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure-elm, stdenv }:
 mkDerivation {
   pname = "pure-contexts";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure-elm ];
   homepage = "github.com/grumply/pure-contexts";
